71c90ee22e96e19aee64d3e413cc8d871cf4dcdc
jasonantwi <jantwi@chicagobooth.edu>
2021-07-21 19:14:27 +0000

    Finished!

--- app/controllers/fortunes_controller.rb
+++ app/controllers/fortunes_controller.rb
@@ -7,8 +7,11 @@ class FortunesController < ApplicationController
     @horoscope = this_zodiac.fetch(:horoscope)
 
     @array_of_numbers = Array.new
-    another_number = rand(1...100)
-    @array_of_numbers.push(another_number)
+    
+    5.times do
+      another_number = rand(1...100)
+      @array_of_numbers.push(another_number)
+    end
     
     render({ :template => "signs/main.html.erb"})
 
--- app/controllers/lottery_controller.rb
+++ app/controllers/lottery_controller.rb
@@ -13,7 +13,6 @@ class LotteryController < ApplicationController
     @array_of_numbers = []
     5.times do
       another_number = rand(1...100)
-      
       @array_of_numbers.push(another_number)
     end
     render({ :template => "lottery_templates/unlucky.html.erb"})
